ac99c9734bb91dac244c29e1680d90d314774967
Tamanna <tamannatam355@gmail.com>
2024-10-04 09:44:04 +0530

    'forth'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   const navigate = useNavigate();
 
   useEffect(()=>{
-    axios.get('https://mpserver-contact.onrender.com/create')
+    axios.get('https://mpserver-contact.onrender.com/create/'+id)
     .then(res => setData(res.data))
     .catch(err => console.log(err));
   },[])
@@ -17,7 +17,7 @@ function Home() {
   const handleDelete=(id)=>{
     const confirm = window.confirm("would you like to Delete?");
     if(confirm){
-      axios.delete('https://mpserver-contact.onrender.com/create'+id)
+      axios.delete('https://mpserver-contact.onrender.com/create/'+id)
       .then (res =>{
         location.reload();
       }).catch(err => console.log(err))
